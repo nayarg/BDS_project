3adf1c8eed2788fd091742450f4eab60904d0f95
Arjun Chintapalli <arjun.ch@gatech.edu>
2017-11-18 19:11:01 -0500

    added shell script

---
+++ runTests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DataPath='../dataset/sample.json'
+OutputPath='shell_output'
+
+#python3 text_clean.py $DataPath $OutputPath
+python3 filter_data.py $OutputPath/TC_saveFolder/wordlists.list $OutputPath/clusters.csv
+python3 parse_counts.py $OutputPath/clusters.csv $OutputPath/clusters_freq_count.counts 'Yelp Review Relevancy' $OutputPath $DataPath
